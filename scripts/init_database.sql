dc20d1fad9272092ffe4afbc9ca92906ee2dd605
Harsha E <138511222+eraganaboina@users.noreply.github.com>
2025-10-22 21:44:16 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,16 @@
+
+/*
+for creating the database of datwarehouse and schema of gold and silver and bronze*/
+
+
+use master;
+go
+create database DataWareHouse;
+go
+use DatawareHouse;
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go 
